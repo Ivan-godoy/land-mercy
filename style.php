59219692607989551a1d19eb7c65681202627b92
igodo <ivangodoy24@gmail.com>
2019-07-29 14:26:11 -0600

    Nueva Pagina

---
+++ style.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/scroll.css">
+<link rel="stylesheet" href="css/estilos.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="css/owl.theme.default.min.css">
+
+<link rel="icon" href="img/logo.png">
